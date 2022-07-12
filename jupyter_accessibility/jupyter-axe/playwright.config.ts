d6a8c3e06caa466b16da6b71d10c658baf07de9f
Gabriel Fouasnon <gabriel@fouasnon.com>
2022-07-12 17:14:54 +0200

    Remove axeOptions in order to use defaults

--- jupyter_accessibility/jupyter-axe/playwright.config.ts
+++ jupyter_accessibility/jupyter-axe/playwright.config.ts
@@ -51,16 +51,7 @@ const config: PlaywrightTestConfig = {
 
     acceptDownloads: true,
 
-    video: "retain-on-failure",
-
-    axeOptions: {
-      runOnly: {
-        type: "tag",
-        // all tags and standards listed here:
-        // https://www.deque.com/axe/core-documentation/api-documentation/#axe-core-tags
-        values: ["wcag2a", "best-practice"],
-      },
-    },
+    video: "retain-on-failure"
   },
 
   /* Configure projects for major browsers */
